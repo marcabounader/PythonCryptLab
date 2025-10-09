9629b92af7cfc87ed825be38060ce1efa4346134
marcabounader <marcabounader92@outlook.com>
2025-10-09 20:12:56 +0300

    altered debug me type

--- debug_me_type.py
+++ debug_me_type.py
@@ -1,4 +1,4 @@
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+age = input("Enter your age: ")
 
-print("hello "+name+ ", next year you will be " + str(age+1))
+print("hello "+name+ ", next year you will be " + str(int(age)+1))
