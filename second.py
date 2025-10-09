9aae3bc56c19e4e95f7aacb3ae4cf51add37e617
marcabounader <marcabounader92@outlook.com>
2025-10-09 19:25:20 +0300

    added my second script

---
+++ second.py
@@ -0,0 +1,14 @@
+print("Please add your name")
+name = input()
+
+#OR
+
+name2 = input("Please give me your other name:\n")
+
+print(name)
+
+if name != "marc":
+    print("this is someone else")
+
+print(name2)
+
