d64df28b8cf5ff3377ba373cb3e96e7b70ebd2c7
marcabounader <marcabounader92@outlook.com>
2025-10-09 20:11:15 +0300

    added the debug scripts

---
+++ debug_me.py
@@ -0,0 +1,5 @@
+name = input("please enter your name:\n")
+
+print("Hello"+" " +name)
+
+print(f"formatted string hello {name}")
---
+++ debug_me_strings.py
@@ -0,0 +1,8 @@
+plain_text="abcd"
+
+cipher_text = "bcde"
+
+key = int(input("what is the key?"))
+print("key is: "* int(key+1))
+
+print(plain_text + cipher_text)
