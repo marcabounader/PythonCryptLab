d08e43caca06f7bc47100ee6306b696e52ec739a
marcabounader <marcabounader92@outlook.com>
2025-10-09 19:41:15 +0300

    i added my third script

---
+++ third.py
@@ -0,0 +1,13 @@
+plain_text = "this is a readable text"
+
+print(plain_text[-1:3:-1])
+
+# 0:t 1:h 2:i
+
+import math
+
+math.factorial(45)
+
+print(plain_text.split())
+print(len(plain_text))
+print(type(plain_text))
