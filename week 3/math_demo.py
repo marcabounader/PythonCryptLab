f62fb1adb430278e996f1d5b4878f1b6e55fb825
marcabounader <instructor@austlab.edu.lb>
2025-10-23 20:25:02 +0300

    added  copy

---
+++ week 3/math_demo.py
@@ -0,0 +1,12 @@
+import math
+
+
+def round_demo():
+    print(8/3)
+    print(math.ceil(8/3))
+    print(math.floor(8/3))
+
+def p():
+    print(float(int(math.pow(2,3))))
+round_demo()
+p()
---
+++ week 3/transpositionEncrypt.py
@@ -0,0 +1,21 @@
+import random
+
+plain_text = "I love abbas, but abbas doesn't love me"
+key = 4
+cipher_text = ""
+row = len(plain_text) // key
+
+def encrypt():
+    global cipher_text
+    pointer = 0
+    while pointer < len(plain_text):
+        cipher_text += cipher_text[pointer]
+        pointer += key
+        if (pointer ==(key-1) and (pointer < (row*key-len(plain_text)-row))):
+            break;
+        if(pointer >= row and pointer != len(plain_text)-1 ):
+            pointer += 1
+    print(cipher_text)
+    
+if __name__ == "__main__":
+    encrypt()
