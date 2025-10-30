699569772590912377a8abcddfb94da1e84c9dd1
marcabounader <instructor@austlab.edu.lb>
2025-10-30 20:20:15 +0200

    add time

--- week 4/read_demo.py
+++ week 4/read_demo.py
@@ -1,4 +1,24 @@
-with open('example.txt') as f:
+"""with open('example.txt') as f:
     lines = f.readlines()
     for l in lines:
-        print(l.strip())
+        print(l.strip())"""
+
+"""import os
+
+print(os.path.exists("example.txtt"))"""
+
+"""s = "secret text"
+
+print(s.upper())
+print(s.capitalize())
+print(s.startswith('s'))
+print(s.endswith("t"))"""
+
+import time
+start = time.time()
+total = sum(range(1,10000))
+while total > 0:
+    total -= 1000
+end = time.time()
+print(f"start: {start}, end: {end}")
+print(f"duration of this script is: {end-start}")
