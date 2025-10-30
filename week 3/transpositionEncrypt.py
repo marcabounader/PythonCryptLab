49d76a986fcc94beffa096f5871fd12f9ab47907
marcabounader <instructor@austlab.edu.lb>
2025-10-30 19:11:49 +0200

    fix the code

--- week 3/transpositionEncrypt.py
+++ week 3/transpositionEncrypt.py
@@ -1,20 +1,27 @@
 import random
-
+from math import ceil
+import sys
 plain_text = "I love abbas, but abbas doesn't love me"
-key = 4
+key = col = 4
 cipher_text = ""
-row = len(plain_text) // key + 1
+row = ceil(len(plain_text) / key)
 
 def encrypt():
     global cipher_text
     pointer = 0
+    counter = 0
     while pointer < len(plain_text):
+        col = 0
+        if (col ==(key-1) and (pointer < (row*key-len(plain_text)-row))):
+            break
         cipher_text += cipher_text[pointer]
         pointer += key
-        if (pointer ==(key-1) and (pointer < (row*key-len(plain_text)-row))):
+        if counter == row - 1:
             break
         if(pointer >= row and pointer != len(plain_text)-1 ):
             pointer += 1
+            col += 1
+            counter = 0
     print(cipher_text)
     
 if __name__ == "__main__":
